b6308b94915da609c2dde14077b008bc6a13cc0a
quang1606 <quangnguyenxuanymnb2004@gmail.com>
2025-05-06 23:06:38 +0700

    up

---
+++ projectLibary/src/main/java/com/example/projectlibary/ProjectLibaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectlibary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLibaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLibaryApplication.class, args);
+    }
+
+}
---
+++ projectLibary/src/test/java/com/example/projectlibary/ProjectLibaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectlibary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLibaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
